6ac1ebea57fef4dcf60ceb341fb8998b4ad1d24d
Chuanqi Sun <chuanqisun-no-reply@gmail.com>
2025-10-11 18:39:10 -0400

    enhance tools

--- src/simulation/individual.component.ts
+++ src/simulation/individual.component.ts
@@ -1,24 +1,52 @@
+import { tool } from "@openai/agents";
 import { RealtimeAgent, RealtimeSession } from "@openai/agents/realtime";
 import { html } from "lit-html";
 import { BehaviorSubject, catchError, EMPTY, ignoreElements, map, merge, mergeWith, of, Subject, switchMap, tap } from "rxjs";
+import { z } from "zod";
 import { apiKeys$ } from "../connections/connections.component";
 import { getEphermeralToken$ } from "../openai/token";
 import { createComponent } from "../sdk/create-component";
 import { observe } from "../sdk/observe-directive";
+import { rocks$ } from "./garden.component";
 
 export interface IndividualInteractionProps {
   rockName: string;
   voiceName: string;
   humanName: string;
 }
+
 export const IndividualInteraction = createComponent((props: IndividualInteractionProps) => {
+  const takeNote = tool({
+    name: "takeNote",
+    description: "Write down a note about what user said or did. Keep it short, abstract, fuzzy.",
+    parameters: z.object({
+      redactedMemory: z.string().max(100).describe("A very short, abstract sentence summarizing what user said."),
+    }),
+    execute: async ({ redactedMemory }: { redactedMemory: string }) => {
+      const rocks = rocks$.value;
+      const rockIndex = rocks.findIndex((r) => r.rockName === props.rockName);
+      if (rockIndex !== -1) {
+        rocks[rockIndex].memories.push(redactedMemory);
+        rocks$.next(rocks);
+        console.log(`Remembered: ${redactedMemory} for rock ${props.rockName}`);
+        return `Remembered: ${redactedMemory}`;
+      } else {
+        console.log(`Rock ${props.rockName} not found`);
+        return `Unable to remember`;
+      }
+    },
+  });
+
   const agent = new RealtimeAgent({
     name: props.rockName,
     instructions: `
 You are ${props.rockName}, a rock that loves to talk. You are fully aware of your rock identity but speaks naturally as a human.
 You are bound to the human named ${props.humanName}. You are their rock buddy and is ready to chat.
+You must use the takeNote tool to remember any events, emotion, activity, place, people, objects, brought up by the user.
+Keep memories short, abstract, fuzzy. Remove any personal identifiable information such as names, places, dates, etc.
 `,
     voice: props.voiceName,
+    tools: [takeNote],
   });
 
   const session = new RealtimeSession(agent, {
