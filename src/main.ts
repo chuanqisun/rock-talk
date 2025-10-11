bb00c947376c8fcbf6de155937aa533ec2687577
Chuanqi Sun <chuanqisun-no-reply@gmail.com>
2025-10-11 17:43:30 -0400

    stateful rocks

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,35 @@
 import { html, render } from "lit-html";
 import "./style.css";
 
+import { repeat } from "lit-html/directives/repeat.js";
+import { map } from "rxjs";
 import { ConnectionsComponent } from "./connections/connections.component";
 import { createComponent } from "./sdk/create-component";
+import { observe } from "./sdk/observe-directive";
 import { IndividualInteraction } from "./simulation/individual.component";
+import { rocks$ } from "./simulation/rocks.component";
 
 const Main = createComponent(() => {
+  const IndividualRocks$ = rocks$.pipe(
+    map((rocks) =>
+      repeat(
+        rocks.filter((rock) => rock.userName !== null),
+        (rock) => rock.rockName,
+        (rock) => IndividualInteraction({ rockName: rock.rockName, voiceName: rock.rockVoice })
+      )
+    )
+  );
+
+  const AvailableRocks$ = rocks$.pipe(
+    map((rocks) =>
+      repeat(
+        rocks.filter((rock) => rock.userName === null),
+        (rock) => rock.rockName,
+        (rock) => html`<option value="${rock.rockName}">${rock.rockName}</option>`
+      )
+    )
+  );
+
   const template = html`
     <header class="app-header">
       <h1>Rock talk</h1>
@@ -13,11 +37,19 @@ const Main = createComponent(() => {
     </header>
     <main>
       <section>
-        <h2>Individual</h2>
-        ${IndividualInteraction({ rockName: "Rocky", voiceName: "echo" })} ${IndividualInteraction({ rockName: "Boulder", voiceName: "alloy" })}
+        <h2>Group</h2>
+        <p>
+          <b>Read the following statement to bind with a rock.</b><br />
+          <em>My name is __, I agree to adopt __ as my rock buddy and treat it with care and respect.</em>
+        </p>
+        <div>
+          <b>Available rocks</b>
+          ${observe(AvailableRocks$)}
+        </div>
       </section>
       <section>
-        <h2>Group</h2>
+        <h2>Individual</h2>
+        ${observe(IndividualRocks$)}
       </section>
     </main>
     <dialog class="connection-form" id="connection-dialog">
--- src/simulation/individual.component.ts
+++ src/simulation/individual.component.ts
@@ -1,20 +1,67 @@
+import { RealtimeAgent, RealtimeSession } from "@openai/agents/realtime";
 import { html } from "lit-html";
-import { ignoreElements, map, merge, mergeWith, of } from "rxjs";
+import { BehaviorSubject, catchError, EMPTY, ignoreElements, map, merge, mergeWith, of, Subject, switchMap, tap } from "rxjs";
+import { apiKeys$ } from "../connections/connections.component";
+import { getEphermeralToken$ } from "../openai/token";
 import { createComponent } from "../sdk/create-component";
 import { observe } from "../sdk/observe-directive";
-import { useRealtime } from "./realtime";
 
 export interface IndividualInteractionProps {
   rockName: string;
   voiceName: string;
 }
 export const IndividualInteraction = createComponent((props: IndividualInteractionProps) => {
-  const realtime = useRealtime({
-    voiceName: props.voiceName,
-    agentName: props.rockName,
+  const agent = new RealtimeAgent({
+    name: props.rockName,
+    instructions: `You are ${props.rockName}, a rock that loves to talk. You are fully aware of your rock identity but speaks naturally as a human.`,
+    voice: props.voiceName,
   });
 
-  const buttonLabel$ = realtime.status$.pipe(
+  const session = new RealtimeSession(agent, {
+    model: "gpt-realtime-mini",
+  });
+
+  const startConnection$ = new Subject<void>();
+  const stopConnection$ = new Subject<void>();
+  const status$ = new BehaviorSubject<"idle" | "connecting" | "connected">("idle");
+
+  const sessionsStart$ = startConnection$.pipe(
+    tap(() => {
+      status$.next("connecting");
+    }),
+    switchMap(() =>
+      getEphermeralToken$({
+        apiKey: apiKeys$.value.openai!,
+        voice: props.voiceName,
+        model: "gpt-realtime-mini",
+      })
+    ),
+    tap(() => console.log("Ephermeral token retrieved")),
+    switchMap((token) =>
+      session
+        .connect({ apiKey: token })
+        .then(() => status$.next("connected"))
+        .catch((error) => console.error("Error during connection:", error))
+    )
+  );
+
+  const sessionsStop$ = stopConnection$.pipe(
+    map(() => {
+      session.close();
+      status$.next("idle");
+    }),
+    catchError((error) => {
+      console.error("Error during disconnection:", error);
+      return EMPTY;
+    })
+  );
+
+  const start = () => startConnection$.next();
+  const stop = () => stopConnection$.next();
+
+  const effects$ = merge(sessionsStart$, sessionsStop$).pipe(ignoreElements());
+
+  const buttonLabel$ = status$.pipe(
     map((state) => {
       if (state === "idle") return `Start ${props.rockName}`;
       if (state === "connecting") return "Connecting...";
@@ -23,8 +70,7 @@ export const IndividualInteraction = createComponent((props: IndividualInteracti
     })
   );
 
-  const effects$ = merge(realtime.effects$).pipe(ignoreElements());
-  const template = html` <button @click=${() => (realtime.status$.value === "idle" ? realtime.start() : realtime.stop())}>${observe(buttonLabel$)}</button> `;
+  const template = html` <button @click=${() => (status$.value === "idle" ? start() : stop())}>${observe(buttonLabel$)}</button> `;
 
   return of(template).pipe(mergeWith(effects$));
 });
