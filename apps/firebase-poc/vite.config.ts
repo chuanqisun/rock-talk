6f0477f09bee8e45845ed3083da4ddebc77e0a1f
Chuanqi Sun <chuanqisun-no-reply@gmail.com>
2025-11-15 16:20:05 -0500

    fix multi page build

---
+++ apps/firebase-poc/vite.config.ts
@@ -0,0 +1,14 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/rock-talk/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        user: resolve(__dirname, "user.html"),
+      },
+    },
+  },
+});
